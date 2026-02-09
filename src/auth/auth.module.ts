a2326090129302358f4e42a4fa9994dfbda37654
Freud <nuttasanz@gmail.com>
2026-02-10 01:51:45 +0700

    feat: implement login route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,29 @@
 import { CreateAdminDto } from './../users/dto/create-admin.dto';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(username: string, pass: string) {
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ accessToken: string }> {
     const user = await this.usersService.findOneWithPassword(username);
 
     if (!user || user.password !== pass) {
       throw new UnauthorizedException('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
     }
 
-    const { password, ...result } = user;
-
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    const payload = { sub: user.id, username: user.username };
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
   }
 
   createAdmin(createAdminDto: CreateAdminDto) {
